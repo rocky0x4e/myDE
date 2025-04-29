dc0e29f73a650599cac9a88190d5c506cdac6422
Rocky <madfatcat88@gmail.com>
2025-04-29 10:14:54 +0700

    update apk installer, direct install when there's only 1 dev

--- rofi-apkInstaller.sh
+++ rofi-apkInstaller.sh
@@ -5,23 +5,26 @@ DELMODE="Delete"
 function installApk {
     file=$1
     adb start-server > /dev/null 2>&1
-    items=()
+    devices=()
     while IFS=$'\n' read -r line; do
         if [[ -z $line ]]; then continue; fi
-        items+=("$line")
+        devices+=("$line")
     done < <(adb devices)
-    items=("${items[@]:1}")
-    menu=''
-    for item in "${items[@]}"; do
-        IFS=$'\t' read -r serial type <<< "$item"
-        if [[ $serial =~ "emulator-" ]]; then ic=android-emu; else ic=android-phone; fi
-        menu+="$serial${I}${ic}\n"
-    done
-
-
-    selects=$(echo -en "${menu}" | rofi  -dmenu -multi-select -icon-theme rofi -theme overlays/thin-side-bar)
-    IFS=$'\n' read -r -d '' -a selects <<< "$selects"
+    devices=("${devices[@]:1}")
+    if [[ ${#devices[@]} == 1 ]]; then
+        IFS=$'\t' read -r selects type <<< "${devices[0]}"
+    else
+        menuDevices=''
+        for item in "${devices[@]}"; do
+            IFS=$'\t' read -r serial type <<< "$item"
+            if [[ $serial =~ "emulator-" ]]; then ic=android-emu; else ic=android-phone; fi
+            menuDevices+="$serial${I}${ic}\n"
+        done
+        selects=$(echo -en "${menuDevices}" | rofi  -dmenu -multi-select -icon-theme rofi -theme overlays/thin-side-bar)
+        IFS=$'\n' read -r -d '' -a selects <<< "$selects"
+    fi
     for s in "${selects[@]}"; do
+        echo -$s-
         r=$(adb -s "$s" install -d "$file" 2>&1)
         notify-send -e -a "APK installer" "Installing..." "$r"
     done
@@ -33,13 +36,15 @@ function browse {
     while IFS=$'\n' read -r line; do
         items+=("$line")
     done < <(ls -1 "$path")
+    IFS=$'\n' sorted=($(sort -r <<<"${items[*]}"))
 
     folder="back${I}back\n"
     apkFiles=""
-    for item in "${items[@]}"; do
+    for item in "${sorted[@]}"; do
         if [[ -d "$path/$item" ]]; then folder+="$item"${I}folder"\n"; fi
         if [[ "$item" =~ ".apk"$ ]]; then apkFiles+="$item"${I}apk-64"\n"; fi
     done
+
     select=$(echo -en "${folder}${apkFiles}" | rofi -dmenu -icon-theme rofi -select test -theme overlays/thin-side-bar)
     echo $select
 }
