827847bc92f7fae7e5fcdcce607ac7e8c94cc275
Rocky <madfatcat88@gmail.com>
2026-05-21 13:32:34 -0500

    exclude response header

--- tools/qbt-add.sh
+++ tools/qbt-add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # QBT_API_TOKEN and QBT_URL must be set as env var
-r=$(curl -i -X POST \
+r=$(curl -X POST \
   -H "Authorization: Bearer ${QBT_API_TOKEN}" \
   -F "urls=$1" \
   ${QBT_URL}/api/v2/torrents/add)
