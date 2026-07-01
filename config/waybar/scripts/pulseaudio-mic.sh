ecf5a07bb18c8f9c291287654ae447572c539ab6
Rocky <madfatcat88@gmail.com>
2026-07-01 18:56:55 -0500

    niri beginning

---
+++ config/waybar/scripts/pulseaudio-mic.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+NPID=0
+isMuted(){
+    m=yes
+    inputSOURCES=($(pactl list short sources | grep input | cut -f1))
+
+    for source in "${inputSOURCES[@]}"; do
+        r=$(pactl get-source-mute $source | cut -d " " -f2)
+        if [[ $r == "no" ]]; then
+            m=no
+            break
+        fi
+    done
+    echo $m
+}
+
+status() {
+    if [ "$(isMuted)" = "yes" ]; then
+        pbIcon=""
+        nIcon="microphone-sensitivity-muted-symbolic"
+        txtStatus=Muted
+    else
+        pbIcon=""
+        nIcon="microphone-sensitivity-high-symbolic"
+        txtStatus=Unmuted
+        inputSOURCES=($(pactl list short sources | grep input | cut -f1))
+        for source in "${inputSOURCES[@]}"; do 
+            pactl set-source-mute "$source" 0
+        done
+    fi
+
+    echo $pbIcon
+    # NPID=$(notify-send --hint string:image-path:$nIcon -a "System Mic" -per $NPID -t 2000 $txtStatus)
+}
+
+listen(){
+    if [[ -z $DEBUG ]]; then
+        scriptName=$(basename $0)
+        others=($(pgrep ${scriptName::15}))
+        for o in ${others[@]}; do
+            if [[ $o -ne $$ ]]; then kill $o; fi
+        done
+    fi
+
+    status
+
+    inputSOURCES=($(pactl list short sources | grep input | cut -f1))
+    grepInScr="source #${inputSOURCES[0]}"
+    for scr in "${inputSOURCES[@]}"; do
+        grepInScr="$grepInScr\\|source #$scr"
+    done
+    LANG=EN; pactl subscribe | while read -r event; do
+        if echo "$event" | grep -q "$grepInScr"; then
+            status
+        fi
+    done
+}
+
+$1
---
+++ rocky-py-tools/lib/fuzzel.py
@@ -0,0 +1,141 @@
+import subprocess as sp
+
+
+class fuzzel:
+    def __init__(self, kwargs=None):
+        self.kwargs = kwargs if kwargs else {}
+        self.items = []
+
+    def makeDmenu(self):
+        self.items = []
+        self.kwargs['--dmenu'] = ""
+        self.kwargs["--icon-theme"] = "rofi"
+        self.kwargs['-i'] = ""
+        self.kwargs['--markup'] = ""
+        return self
+
+    def setInputBarChildren(self, childrend):
+        self.kwargs["-theme+inputbar+children"] = childrend
+        return self
+
+    def setTheme(self, theme):
+        self.kwargs["-theme"] = theme
+        return self
+
+    def setIconTheme(self, iconTheme):
+        self.kwargs["--icon-theme"] = iconTheme
+        return self
+
+    def setPrompt(self, prompt):
+        self.kwargs["-p"] = prompt
+        return self
+
+    def setWindowWidth(self, windowWidth):
+        self.kwargs["-theme+window+width"] = windowWidth
+        return self
+
+    def sortDmenu(self, reverse=False):
+        self.items = sorted(self.items, reverse=reverse)
+        return self
+
+    def addItem(self, item, icon=None, index=-1):
+        if icon:
+            item = f"{item}{self.makeIcon(icon)}"
+        if index == -1:
+            self.items.append(item)
+            return self
+        self.items.insert(index, item)
+        return self
+
+    def makeTable(self, numOfCol):
+        self.kwargs['--dmenu'] = ""
+        self.kwargs["--icon-theme"] = "rofi"
+        self.kwargs['-i'] = ""
+        self.items = [[] for i in range(numOfCol)]
+        return self
+
+    def addTableItem(self, item, icon=None, column=0):
+        self.items[column].append(f"{item}{self.makeIcon(icon)}")
+        return self
+
+    def addPseudoTableIcon(self, icon):
+        self.items[-1][-1] += self.makeIcon(icon)
+
+    def fmtPseudoTable(self, colSeparator="〱"):
+        tableData = self.items
+        self.items = []
+        try:
+            colWidth = [max([len(item) for item in col]) for col in tableData]
+        except ValueError:
+            return self
+        for lineIndex in range(len(tableData[0])):
+            item = ''
+            for colIndex in range(len(tableData)):
+                item += tableData[colIndex][lineIndex].ljust(colWidth[colIndex]) + f" {colSeparator} "
+            self.items.append(item.rstrip(f" {colSeparator} "))
+
+        return self
+
+    def rJustifyCol(self, col):
+        maxChar = max([len(x) for x in self.items[col]])
+        self.items[col] = [x.rjust(maxChar) for x in self.items[col]]
+        return self
+
+    def run(self, additionArgs=None):
+        additionArgs = {} if additionArgs is None else additionArgs
+        try:
+            menu = "\n".join(self.items)
+        except TypeError as e:
+            items = []
+            for col in self.items:
+                items.extend(col)
+            menu = "\n".join(items)
+            colNum = str(len(self.items))
+            lineNum = str(max([len(x) for x in self.items]))
+            self.kwargs['-theme+listview+columns'] = colNum
+            self.kwargs['-theme+listview+lines'] = lineNum
+
+        allKwArgs = {**self.kwargs, **additionArgs}
+        allArgs = []
+        for k, v in allKwArgs.items():
+            allArgs.append(k)
+            allArgs.append(v) if v else None
+
+        try:
+            print(1)
+            return sp.check_output(["fuzzel", *allArgs], input=menu.encode()).decode().strip()
+        except sp.CalledProcessError as e:
+            print(e)
+            print(menu, "|", " ".join(["fuzzel", *allArgs]))
+            exit(0)
+
+    def makeIcon(self, iconName):
+        return f"\x00icon\x1f{iconName}" if iconName else ""
+
+    def isMenuEmpty(self):
+        return self.items == [] or self.items[0] == []
+
+    def addSeparator(self, length=40, text='', dash='-', icon="zigzag"):
+        self.addItem(*fuzzel.separator(length, text, dash, icon))
+        return self
+
+    def addMesg(self, mesg):
+        self.kwargs['-mesg'] = mesg
+        return self
+
+    @staticmethod
+    def separator(length=40, text='', dash='-', icon="zigzag"):
+        if text:
+            l = len(text)
+            odd = l % 2 == 1
+            dashCount = int((length - l)/2 - 1)
+            dashesLeft = f"{dash * dashCount}"
+            dashesRight = dashesLeft + dash if odd else dashesLeft
+            print(f"\n{text}:{odd}\n{dashesLeft}\n{dashesRight}")
+            return (f"{dashesLeft} {text} {dashesRight}", "zigzag")
+        return dash * length, icon
+
+    @staticmethod
+    def yesNo(msg="Are you sure?"):
+        yesNo = fuzzel().makeDmenu().setTheme("overlays/center-yes-no").setPrompt(msg)
+        return yesNo.addItem("Yes", "yes").addItem("No", "no").run()
