6307abd679e917e7e15692a7547950315c035612
Rocky <madfatcat88@gmail.com>
2026-08-01 20:24:05 -0500

    clean up

--- config/waybar/scripts/browser.py
+++ config/waybar/scripts/browser.py
@@ -3,52 +3,66 @@
 import subprocess as sp
 import json
 import sys
+import os
 
 
-browsers = {
+HOME = os.getenv("HOME")
+TOOLTIP = "Click: cycle/open browser\nRight click: open other browser\nMiddle click: change default browser"
+
+BROWSERS = {
     "brave-browser.desktop": {
         "xdgName": "brave-browser.desktop",
         "name": "Brave",
         "app-id": "brave-browser",
+        "icon": f"{HOME}/.local/share/icons/wmicons/512x512/apps/Brave.png"
     },
     "google-chrome.desktop": {
         "xdgName": "google-chrome.desktop",
         "name": "Chrome",
         "app-id": "Google-chrome",
+        "icon": f"{HOME}/.local/share/icons/wmicons/512x512/apps/Chrome.png"
     },
     "firefox.desktop": {
         "xdgName": "firefox.desktop",
         "name": "Firefox",
         "app-id": "firefox",
+        "icon": f"{HOME}/.local/share/icons/wmicons/512x512/apps/Firefox.png"
     }
 }
 unknow = {
     "xdgName": "",
     "name": "Unknown",
     "app-id": "",
+    "icon": f"{HOME}/.local/share/icons/wmicons/512x512/apps/shrug.png"
 }
 DEX_DIR = "/usr/share/applications"
 
 
 def findByName(name):
-    for o in browsers.values():
+    for o in BROWSERS.values():
         if name == o['name']:
             return o
     return {}
 
 
 def getCurrentBrowser():
-    return sp.run(["xdg-settings", "get", "default-web-browser"], capture_output=True, text=True).stdout.strip()
+    xdgCB = sp.run(["xdg-settings", "get", "default-web-browser"], capture_output=True, text=True).stdout.strip()
+    return BROWSERS.get(xdgCB, unknow)
 
 
 def show():
-    print(json.dumps({"class": browsers.get(getCurrentBrowser(), unknow)["name"],
-                      "tooltip": "Click: cycle/open browser \nRight click: open other browser\nMiddle click: change default browser"}))
+    curr = getCurrentBrowser()
+    print(json.dumps({"class": curr["name"], "tooltip": TOOLTIP}))
+
+
+def showIcon():
+    curr = getCurrentBrowser()
+    print(json.dumps([{"path": curr["icon"], "marker": "normal", "tooltip": TOOLTIP}]))
 
 
 def open(name=''):
     if not name:
-        curr = browsers.get(getCurrentBrowser(), unknow)
+        curr = getCurrentBrowser()
     else:
         curr = findByName(name)
 
@@ -68,9 +82,9 @@ def open(name=''):
 
 def selectOpen():
     from lib.fuzzel import fuzzel
-    curr = browsers.get(getCurrentBrowser(), unknow)
+    curr = getCurrentBrowser()
     fz = fuzzel({'--select': curr['name']}).makeDmenu().setAnchor("top").setMesg("Open browser ").hidePrompt()
-    for k, o in browsers.items():
+    for k, o in BROWSERS.items():
         fz.addItem(o['name'], o['name'])
     select = fz.run()
     open(select)
@@ -78,21 +92,24 @@ def selectOpen():
 
 def changeDefault():
     from lib.fuzzel import fuzzel
-    curr = browsers.get(getCurrentBrowser(), unknow)
+    curr = getCurrentBrowser()
     fz = fuzzel({'--select': curr['name']}).makeDmenu().setAnchor("top").setMesg("Select default browser ").hidePrompt()
-    for k, o in browsers.items():
+    for k, o in BROWSERS.items():
         fz.addItem(o['name'], o['name'])
     select = fz.run()
     newBr = findByName(select)['xdgName']
-    sp.call(["xdg-settings", "set", "default-web-browser", newBr])
+    sp.check_call(["xdg-settings", "set", "default-web-browser", newBr])
 
 
 ACTION = sys.argv[1]
 if ACTION == "show":
     show()
+elif ACTION == "showIcon":
+    showIcon()
 elif ACTION == "change":
     changeDefault()
 elif ACTION == "open":
     open()
 elif ACTION == "select":
     selectOpen()
+sp.call(["pkill", "-n", "waybar", "--signal", "41"])
