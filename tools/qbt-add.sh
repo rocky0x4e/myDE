38f7d044b6b490628828fc333ed5b4850550bb56
Rocky <madfatcat88@gmail.com>
2026-08-06 22:36:27 -0500

    fix notification

--- tools/qbt-add.sh
+++ tools/qbt-add.sh
@@ -4,5 +4,5 @@ r=$(curl -X POST \
   -H "Authorization: Bearer ${QBT_API_TOKEN}" \
   -F "urls=$1" \
   ${QBT_URL}/api/v2/torrents/add)
-dunstify -a "Torrent handler" -t 3000 "$r" "$QBT_URL"
+notify-send -a "Torrent handler" -t 3000 "$r" "$QBT_URL"
 
